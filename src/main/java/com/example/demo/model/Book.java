0cde968c5f87982b50019a70c833278dbd5ad329
Refael <Refaeltr88@gmail.com>
2022-12-12 12:14:27 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+}
